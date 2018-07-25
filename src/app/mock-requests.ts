a860ab9cc73fb2c75e92b2212c868edb757aeb70
PTAdrian1997 <poptifreaadrian@gmail.com>
2018-07-25 11:04:58 +0300

    - Added the articleDescription property to the Request class;

--- src/app/mock-requests.ts
+++ src/app/mock-requests.ts
@@ -1,7 +1,7 @@
 import {Request} from './request';
 
 export const REQUESTS: Request[] = [
-	{ hash: 'hash1ffs', documentName: 'document1', proposerName: 'accountName1333', authorName: 'Ion Anton', contentLink: 'cL1'},
-	{ hash: 'hash2', documentName: 'document2', proposerName: 'accountName2', authorName: 'Mihai Eminescu', contentLink: 'cL2'},
-	{ hash: 'hash3', documentName: 'document3', proposerName: 'accountName3', authorName: 'Vrabie Ulariu', contentLink: 'cL3'}
+	{ hash: 'hash1ffs', documentName: 'document1', proposerName: 'accountName1333', authorName: 'Ion Anton', contentLink: 'cL1', articleDescription: 'aD1'},
+	{ hash: 'hash2', documentName: 'document2', proposerName: 'accountName2', authorName: 'Mihai Eminescu', contentLink: 'cL2', articleDescription: 'aD2'},
+	{ hash: 'hash3', documentName: 'document3', proposerName: 'accountName3', authorName: 'Vrabie Ulariu', contentLink: 'cL3', articleDescription: 'aD3'}
 ];
